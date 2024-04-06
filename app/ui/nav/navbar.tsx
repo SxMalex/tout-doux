c47735f322f76fa6fd455a424ed0177e3cdd1d92
SxMalex <perrin.alexandre89@gmail.com>
2024-04-06 09:52:05 +0200

    fix burger display

--- app/ui/nav/navbar.tsx
+++ app/ui/nav/navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
                 }}>
                     {/* hamburger button */}
                     <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-                    <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
                     <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
 
